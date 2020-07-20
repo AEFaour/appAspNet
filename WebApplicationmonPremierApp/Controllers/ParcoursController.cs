e30f2f29b876c3cc2031740e155aa9921576b843
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-07-20 11:45:19 +0200

    app asp.net init

---
+++ WebApplicationmonPremierApp/Controllers/ParcoursController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationmonPremierApp.Controllers
+{
+    public class ParcoursController : Controller
+    {
+        public ActionResult Liste()
+        {
+            // Les actions du controler -- méthode
+            return View();
+            // ce qui suppos que nous avons une Vue qui s'appelle Liste
+            // et qui se trouve dans le rep /Views/parcours
+        }
+        // index qui route vers une autre Vue
+
+        public ActionResult Index()
+        {
+            return View("Toto");
+        }
+    }
+}
